8d9d9844813bc4f4aace3f7864e619c9ad5f5882
Aldo David Monter Cedeño <soportegmtechsolutions@gmail.com>
2025-12-09 16:43:53 -0600

    Actualizar URL del Backend en el Frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // src/utils/api.js
 // AHORA: Usamos localhost para desarrollo
 // LUEGO: Cambiaremos esto por la URL que nos dé Render (ej: https://mi-api.onrender.com)
-export const API_URL = "https://geo-control-dom-qnsj.onrender.com";
+export const API_URL = "https://geo-control-api.onrender.com";
