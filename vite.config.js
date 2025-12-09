a64e3d502c945fdd6ee7ea3c222de4a0f3707ac6
Aldo David Monter Cedeño <soportegmtechsolutions@gmail.com>
2025-12-08 19:46:45 -0600

    Fix jspdf config and deps

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  // 1. Aseguramos que jspdf y autotable se precarguen (opcional, pero recomendado)
-  optimizeDeps: {
-    include: ['jspdf', 'jspdf-autotable'] 
-  },
-  // 2. FORZAMOS EL COMPORTAMIENTO DE CARGA GLOBAL
   build: {
-    // Definimos la configuración para Rollup (el bundler de Vite en build)
+    // Esto asegura que las librerías grandes se dividan bien, pero NO las excluye
+    chunkSizeWarningLimit: 1600, 
     rollupOptions: {
-      // Le decimos a Rollup que "jspdf" es una dependencia externa...
-      external: ['jspdf'], 
       output: {
-        // ... y que su contenido se encuentra en la variable global 'jsPDF'.
-        globals: {
-          jspdf: 'jsPDF' 
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
         }
       }
     }
